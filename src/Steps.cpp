92c31fbe146c6b41f753e9c720c3b42d0db29a96
meshubh <meshubh@missionrnd.com>
2016-03-30 19:05:28 +0530

    Submit Commit

--- src/Steps.cpp
+++ src/Steps.cpp
@@ -21,9 +21,70 @@
 
 */
 #include "stdafx.h"
+#include<stdio.h>
+#include<malloc.h>
 
+int cal_2count(int arr[], int len)
+{
+	int i = 0, count_2 = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (arr[i] == 2)
+			count_2++;
+	}
+	return count_2;
+}
+int fact(int num)
+{
+	if (num == 0)
+		return 1;
+	else
+	{
+		int prod = 1;
+		while (num > 0)
+		{
+			prod *= num;
+			num--;
+		}
+		return prod;
+	}
+}
+int generateSeqUsing1and2(int x, int arr[], int curr_sum, int curr_idx, int *ans)
+{
+	// If current sum is equal to x, then we found a sequence
+	if (curr_sum == x)
+	{
+		int x1;
+		x1 = cal_2count(arr, curr_idx);
+		int numerator = fact(curr_idx);
+		int den1 = fact(x1);
+		int den2 = fact((curr_idx - x1));
+		int denominator = den1*den2;
+		int y = numerator / denominator;
+		*ans = *ans + y;
+		return *ans;
+	}
+
+	int num = 1;
+	while (num <= x - curr_sum && (curr_idx == 0 || num <= arr[curr_idx - 1]))
+	{
+		arr[curr_idx] = num;
+		generateSeqUsing1and2(x, arr, curr_sum + num, curr_idx + 1, ans);
+		num++;
+		if (num == 3)
+		{
+			return *ans;
+		}
+	}
+	return *ans;
+}
 
 int get_steps(int s)
 {
-	return 0;
+	if (s == 0)
+		return 0;
+	int an = 0;
+	int* arr = (int*)malloc(s * sizeof(int));
+	int sol = generateSeqUsing1and2(s, arr, 0, 0, &an);
+	return sol;
 }
