4567cca506c4be95db57d84bac761e2eb53434fe
meshubh <meshubh@missionrnd.com>
2016-04-09 17:50:11 +0530

    Submit Commit

--- src/Maze.cpp
+++ src/Maze.cpp
@@ -34,9 +34,80 @@ more parameters .
 */
 
 #include<stdlib.h>
-
-
+#include<stdio.h>
+#include<malloc.h>
+int clear_path(int *maze, int rows, int columns, int x, int y)
+{
+	if (x >= 0 && x < rows && y >= 0 && y < columns)
+	{
+		if(*((maze + x*columns) + y) == 1)
+		{
+			return 1;
+		}
+		return 0;
+	}
+	return 0;
+}
+int check_path(int *maze, int rows, int columns, int x1, int y1, int x2, int y2, int *visited)
+{
+	if (x1 == x2 && y1 == y2)
+	{
+		*((visited + x1*columns) + y1) = 1;
+		return 1;
+	}
+	if (clear_path(maze, rows, columns, x1, y1) == 1)
+	{
+		*((visited + x1*columns) + y1) = 1;
+		if (*((visited + (x1 + 1)*columns) + y1) == 0)
+		{
+			if (check_path(maze, rows, columns, x1 + 1, y1, x2, y2, visited) == 1)
+			{
+				return 1;
+			}
+		}
+		if (*((visited + x1*columns) + (y1 + 1)) == 0)
+		{
+			if (check_path(maze, rows, columns, x1, y1 + 1, x2, y2, visited) == 1)
+			{
+				return 1;
+			}
+		}
+		if (*((visited + (x1 - 1)*columns) + y1) == 0)
+		{
+			if (check_path(maze, rows, columns, x1 - 1, y1, x2, y2, visited) == 1)
+			{
+				return 1;
+			}
+		}
+		if (*((visited + x1*columns) + (y1 - 1)) == 0)
+		{
+			if (check_path(maze, rows, columns, x1, y1 - 1, x2, y2, visited) == 1)
+			{
+				return 1;
+			}
+		}
+		*((visited + x1*columns) + y1) = 0;
+		return 0;
+	}
+	return 0;
+}
 int path_exists(int *maze, int rows, int columns, int x1, int y1, int x2, int y2)
 {
-	return 1;
+	if (rows < 0 || columns < 0 || x1 < 0 || y1 < 0 || x2 > rows || y2 > columns)
+		return 0;
+	int *visited = (int *)malloc(rows * columns * sizeof(int));
+	int i, j;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < columns; j++)
+		{
+			*((visited + i*columns) + j) = 0;
+		}
+	}
+	int x;
+	x = check_path(maze, rows, columns, x1, y1, x2, y2, visited);
+	if (*((maze + x2*columns) + y2) == 1 && *((visited + x2*columns) + y2) == 1)
+		return 1;
+	else
+		return 0;
 }
